9e8c3536324df685af176e02e730b529b689ddf2
lashkarava03-creator <lashkarava03@gmail.com>
2025-12-22 14:44:30 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function resize() {
 
   center.x = canvas.width / 2;
   center.y = canvas.height / 2;
+  textPoints = generateTextPoints("MuYuDo");
+
 }
 
 window.addEventListener("resize", resize);
@@ -23,6 +25,9 @@ resize();
 // ნაწილაკები
 const particles = [];
 const particleCount = 120;
+let mode = "FLOW"; // FLOW | FORM_TEXT | DISPERSE
+let textPoints = [];
+let timer = 0;
 
 for (let i = 0; i < particleCount; i++) {
   particles.push({
@@ -40,12 +45,27 @@ function animate() {
 
   particles.forEach((p, i) => {
     // მიზიდვა ცენტრისკენ
-    const dx = center.x - p.x;
-    const dy = center.y - p.y;
+   if (mode === "FORM_TEXT" && textPoints[i]) {
+  const tx = textPoints[i].x;
+  const ty = textPoints[i].y;
 
+  p.vx += (tx - p.x) * 0.01;
+  p.vy += (ty - p.y) * 0.01;
+}
+else if (mode === "DISPERSE") {
+  p.vx += (Math.random() - 0.5) * 0.5;
+  p.vy += (Math.random() - 0.5) * 0.5;
+}
+else {
+  const dx = center.x - p.x;
+  const dy = center.y - p.y;
+  const dist = Math.hypot(dx, dy);
+
+  if (dist > 140) {
     p.vx += dx * 0.00002;
     p.vy += dy * 0.00002;
-
+  
+}
     p.x += p.vx;
     p.y += p.vy;
 
@@ -76,8 +96,43 @@ function animate() {
       }
     }
   });
+timer++;
+
+if (timer === 300) mode = "FORM_TEXT";
+if (timer === 600) mode = "DISPERSE";
+if (timer === 900) {
+  mode = "FLOW";
+  timer = 0;
+}
 
   requestAnimationFrame(animate);
+}
+function generateTextPoints(text) {
+  const tempCanvas = document.createElement("canvas");
+  const tctx = tempCanvas.getContext("2d");
+
+  tempCanvas.width = canvas.width;
+  tempCanvas.height = canvas.height;
+
+  tctx.fillStyle = "white";
+  tctx.textAlign = "center";
+  tctx.textBaseline = "middle";
+  tctx.font = "bold 120px Arial";
+
+  tctx.fillText(text, canvas.width / 2, canvas.height / 2);
+
+  const imageData = tctx.getImageData(0, 0, canvas.width, canvas.height);
+  const points = [];
+
+  for (let y = 0; y < canvas.height; y += 6) {
+    for (let x = 0; x < canvas.width; x += 6) {
+      const index = (y * canvas.width + x) * 4;
+      if (imageData.data[index + 3] > 128) {
+        points.push({ x, y });
+      }
+    }
+  }
+  return points;
 }
 
 // გაშვება
